de29358d65056958e5e891b956ce1cb3aae689bd
Sairama <sairamaj@gmail.com>
2019-09-18 14:46:24 -0700

    Added data entry form view

---
+++ Utils.Core/ViewModels/NameValueViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Utils.Core.ViewModels
+{
+    public class NameValueViewModel : CoreViewModel
+    {
+        public NameValueViewModel(string name, string value)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Value = value;
+        }
+
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ Utils.Core/Views/DataEntryFormView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Utils.Core.Views
+{
+    /// <summary>
+    /// Interaction logic for FormView.xaml
+    /// </summary>
+    public partial class DataEntryFormView : UserControl
+    {
+        public DataEntryFormView()
+        {
+            InitializeComponent();
+        }
+    }
+}
